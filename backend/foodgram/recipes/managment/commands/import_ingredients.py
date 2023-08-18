efb6614db98c88bcc1f378a6fba98be7d6ff32eb
Gor Galstyan <gorqwertyuiop@mail.ru>
2023-08-18 16:53:48 +0300

    default back

---
+++ backend/foodgram/recipes/managment/commands/import_ingredients.py
@@ -0,0 +1,12 @@
+from recipes.models import Ingredient
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        with open("ingredients.csv") as f:
+            for line in f:
+                ing = line.split(',')
+                Ingredient.objects.get_or_create(
+                    name=ing[0],
+                    measurement_unit=ing[1])
